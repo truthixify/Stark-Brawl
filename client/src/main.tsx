a4780d938cb55c17a00f3c94e6558370243019c2
KevinMB0220 <130603817+KevinMB0220@users.noreply.github.com>
2025-03-24 13:43:11 -0600

    feat: init pages

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,55 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
 import App from "./App.tsx";
+import "./index.css"; 
 
-// Dojo related imports
-import { init } from "@dojoengine/sdk";
-import { DojoSdkProvider } from "@dojoengine/sdk/react";
-import { SchemaType } from "./typescript/models.gen.ts";
-import { setupWorld } from "./typescript/contracts.gen.ts";
-
-import "./index.css";
-import { dojoConfig } from "../dojoConfig.ts";
-import StarknetProvider from "./starknet-provider.tsx";
-
-/**
- * Initializes and bootstraps the Dojo application.
- * Sets up the SDK, burner manager, and renders the root component.
- *
- * @throws {Error} If initialization fails
- */
-async function main() {
-    const sdk = await init<SchemaType>({
-        client: {
-            rpcUrl: dojoConfig.rpcUrl,
-            toriiUrl: dojoConfig.toriiUrl,
-            relayUrl: dojoConfig.relayUrl,
-            worldAddress: dojoConfig.manifest.world.address,
-        },
-        domain: {
-            name: "WORLD_NAME",
-            version: "1.0",
-            chainId: "KATANA",
-            revision: "1",
-        },
-    });
-
-    createRoot(document.getElementById("root")!).render(
-        <StrictMode>
-            <DojoSdkProvider
-                sdk={sdk}
-                dojoConfig={dojoConfig}
-                clientFn={setupWorld}
-            >
-                <StarknetProvider>
-                    <App />
-                </StarknetProvider>
-            </DojoSdkProvider>
-        </StrictMode>
-    );
-}
-
-main().catch((error) => {
-    console.error("Failed to initialize the application:", error);
-});
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
---
+++ client/src/pages/shop.tsx
@@ -0,0 +1,8 @@
+export default function ShopPage() {
+    return (
+      <div className="p-6 text-white">
+        <h1 className="text-3xl font-bold mb-4">Tienda de Objetos</h1>
+      </div>
+    );
+  }
+  
