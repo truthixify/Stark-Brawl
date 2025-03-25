2f646988a98d09981eaf350e1e9dd8f52b7d89ee
Oseec <113547766+Oseec@users.noreply.github.com>
2025-03-24 22:49:35 -0600

    "resolve conflicts with merge"

--- client/src/pages/shop.tsx
+++ client/src/pages/shop.tsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
+import ShopTabs from "../components/shop/shop-tabs"; //Importamos ShopTabs
+
 export default function ShopPage() {
+  const [activeTab, setActiveTab] = useState("featured");
+
   return (
-    <div className="min-h-screen bg-gray-900 p-6 text-white">
-    
+    <div className="p-6 text-white bg-gradient-to-r from-purple-700 via-purple-500 to-orange-500 min-h-screen">
+      <h1 className="text-4xl font-bold mb-4">Shop</h1>
+      {/* Pestañas de navegación */}
+      <ShopTabs activeTab={activeTab} setActiveTab={setActiveTab} />
     </div>
   );
 }
