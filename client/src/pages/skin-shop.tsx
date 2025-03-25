d2b61d8627dcc5a7b9760b5d92abcf0ef5053c50
Dario Zamora Rojas <dariozamora0731@gmail.com>
2025-03-25 12:27:16 -0600

    feat: add-taps

--- client/src/pages/skin-shop.tsx
+++ client/src/pages/skin-shop.tsx
@@ -1,7 +1,9 @@
 import SkinCard from "@/components/skin-shop/skin-card";
+import SkinTabs from "@/components/skin-shop/skin-tabs";
 export default function SkinShopPage() {
     return (
       <div className="p-6 text-blue-900">
+   <SkinTabs/>
         <SkinCard/>
       </div>
     );
