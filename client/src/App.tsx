b4660c009c143a1264d17017ad3efe8ae3f001fc
Ryzen XP <iron4540@gmail.com>
2025-03-29 23:18:58 +0530

    minner

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,12 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import ShopPage from "./pages/shop";
 import SocialPage from "./pages/social";
-import EventsLayout from "./components/shop/EventLayout";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/shop" element={<EventsLayout />} />
+        <Route path="/shop" element={<ShopPage />} />
         <Route path="/" element={<Navigate to="/shop" replace />} />
         <Route path="/social" element={<SocialPage />} />
       </Routes>
