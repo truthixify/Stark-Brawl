d4abbe46c5dbc64fd3c8acbcd0eea54481f737af
Fran19-09 <francinnim8@gmail.com>
2025-03-29 11:55:04 -0600

    feat: create events page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,18 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import ShopPage from './pages/shop';
-import SocialPage from './pages/social';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import ShopPage from "./pages/shop"
+import SocialPage from "./pages/social"
+import EventsPage from "./pages/events"
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/shop' element={<ShopPage />} />
-        <Route path='/' element={<Navigate to='/shop' replace />} />
-        <Route path='/social' element={<SocialPage />} />
+        <Route path="/shop" element={<ShopPage />} />
+        <Route path="/" element={<Navigate to="/shop" replace />} />
+        <Route path="/social" element={<SocialPage />} />
+        <Route path="/events" element={<EventsPage />} />
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
+
