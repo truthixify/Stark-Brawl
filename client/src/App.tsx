bca4c9f5fdf11f7a42a942c5b4e69f764053af42
manuelcuesta2005 <macuba2005@gmail.com>
2025-07-01 19:44:54 +0200

    implement interface game

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -22,6 +22,7 @@ import {
 } from "@starknet-react/core";
 import { mainnet, sepolia } from "@starknet-react/chains";
 import cartridgeConnector from "./CartredgeConnector";
+import MainMenu from "./components/ui/MainMenu";
 
 export default function App() {
   const { connectors } = useInjectedConnectors({
@@ -48,17 +49,8 @@ export default function App() {
           <Route path="/club" element={<ClubPage />} />
           <Route path="/brawlers" element={<Brawlers />} />
           <Route path="/battle-log" element={<BattleLogPage />} />
-          <Route
-            path="/game"
-            element={
-              <MainGameScene
-                mapSrc="/game_background_1.png"
-                enemyId={1}
-                enemyInitialX={100}
-                enemyInitialY={150}
-              />
-            }
-          />
+          <Route path="/game" element={<MainGameScene />} />
+          <Route path="/interface" element={<MainMenu />} />
           <Route path="/" element={<Navigate to="/home" replace />} />
         </Routes>
       </BrowserRouter>
